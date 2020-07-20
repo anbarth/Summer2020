aa6ed5c0aba5c10fbde69dc745feeb210ee99157
Vlcek Lab Guest 3 <guest3@erwin3>
2020-07-20 10:36:18 -0700

    anooother heat map?

--- interceptsHeatMap.py
+++ interceptsHeatMap.py
@@ -20,7 +20,9 @@ right = 20
 dx = 0.05
 Nmax = 5000
 #sampleSize = 50
-trials = 100
+trials = 50
+#numTrialGroups = 10
+#trialGroupSize = 10
 
 # dimension of discretized position space
 D = int((right-left)/dx)
@@ -48,17 +50,27 @@ for i in range(trials):
 
 intercepts = np.zeros((nMax+1,nMax+1))
 intercept_errs = np.zeros((nMax+1,nMax+1))
+slopes = np.zeros((nMax+1,nMax+1))
+slope_errs = np.zeros((nMax+1,nMax+1))
+r_sqs = np.zeros((nMax+1,nMax+1))
+
 lnN = [np.log(N) for N in range(1,Nmax+1)]
 lnSigma = np.zeros((nMax+1,nMax+1,Nmax))
 for n1 in range(nMax+1):
     for n2 in range(n1,nMax+1):
         for N in range(1,Nmax+1):
             lnSigma[n1][n2][N-1] = np.log(stdev(overlaps[n1][n2][N-1]))
-        
+            #theseSigs = np.zeros((numTrialGroups))
+            #for i in range(numTrialGroups):
+            #    theseSigs[i] = stdev(overlaps[n1][n2][N-1][trialGroupSize*i:trialGroupSize*(i+1)])
+            #lnSigma[n1][n2][N-1] = mean(theseSigs)
+            
         (slope, intercept, r_sq, slope_err, intercept_err) = regress(lnN, lnSigma[n1][n2])
         intercepts[n1][n2] = intercept
         intercept_errs[n1][n2] = intercept_err
-
+        slopes[n1][n2] = slope
+        slope_errs[n1][n2] = slope_err
+        r_sqs[n1][n2] = r_sq
 
 with open('heatmap.csv','w') as csvFile:
     writer = csv.writer(csvFile,delimiter=',')
@@ -73,6 +85,20 @@ with open('heatmap.csv','w') as csvFile:
     writer.writerow(['intercept errors'])
     for i in range(len(intercept_errs)):
         writer.writerow(intercept_errs[i])
+
+    writer.writerow(['slopes'])
+    for i in range(len(slopes)):   
+        writer.writerow(slopes[i])
+
+    writer.writerow(['slope errors'])
+    for i in range(len(slope_errs)):
+        writer.writerow(slope_errs[i])
+
+    writer.writerow(['R^2s'])
+    for i in range(len(r_sqs)):
+        writer.writerow(r_sqs[i])
+
+
 
 toc = time.time()
 print('runtime (s): '+str(toc-tic))
