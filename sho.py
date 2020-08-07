b27248959ecd1d8d916d72470f1c770e705b71ad
anbarth <anna2nsb@gmail.com>
2020-08-07 13:39:48 -0400

    flat defect states

--- sho.py
+++ sho.py
@@ -48,6 +48,46 @@ def cakeEigenstates(d1,d2,w1,w2,center,left,right,dx,nMin,nMax,dx_solve):
 
     return (E,psi)
 
+def flatDefectEigenstates(depth,width,center,left,right,dx,nMin,nMax,dx_solve):
+    # dimension of discretized position space in which we SOLVE
+    D_solve = int((right-left)/dx_solve)
+    wing = width/2.0
+
+    # define U
+    U = np.zeros((D_solve))
+    x = left
+    for i in range(D_solve):
+        pot = x*x
+        if x <= center+wing and x >= center-wing:
+            pot = -1.0*depth
+        U[i] = pot
+        x += dx_solve
+
+    # construct H
+    diag = U + 2/(dx_solve*dx_solve)
+    tridiag = [-1.0/(dx_solve*dx_solve)] * (D_solve-1)
+
+    (E,psi_smooth) = eigh_tridiagonal(diag,tridiag,select='i',select_range=(nMin,nMax))
+    psi_smooth = np.transpose(psi_smooth)
+
+    # now that it's been solved in the less discretized space,
+    # transfer the wavefxns into the more discretized space
+    D = int((right-left)/dx)
+    psi = np.zeros((nMax-nMin+1,D))
+    for i in range(D):
+        pos = int(i*dx * 1.0/dx_solve)
+        for j in range(len(psi)):
+            psi[j][i] = psi_smooth[j][pos]
+
+    # finally, normalize each wavefxn
+    for i in range(len(psi)):
+        norm2 = 0
+        for j in range(D):
+            norm2 += psi[i][j]*psi[i][j]
+        # normalize
+        psi[i] = psi[i]*(1/math.sqrt(norm2))
+
+    return (E,psi)
 
 def defectEigenstates(depth,width,center,left,right,dx,nMin,nMax,dx_solve):
     # dimension of discretized position space in which we SOLVE
@@ -66,14 +106,6 @@ def defectEigenstates(depth,width,center,left,right,dx,nMin,nMax,dx_solve):
         x += dx_solve
 
     # construct H
-    '''ham = np.zeros((D_solve,D_solve))
-    for i in range(D_solve):
-        # diagonal terms: U (potential)+2/dx^2 (kinetic)
-        ham[i][i] = U[i]+2/(dx_solve*dx_solve)
-        # tridiagonal terms: -1/dx^2 (kinetic)
-        if i > 0:
-            ham[i][i-1] = -1/(dx_solve*dx_solve)
-            ham[i-1][i] = -1/(dx_solve*dx_solve)'''
     diag = U + 2/(dx_solve*dx_solve)
     tridiag = [-1.0/(dx_solve*dx_solve)] * (D_solve-1)
 
